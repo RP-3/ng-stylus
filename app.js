f4c80e59a14563b92662ac6faaa013396b24e73b
Rohan <sarith21@gmail.com>
2014-07-28 23:00:19 -0700

    Binds mousedown event to canvas, storing initial coordinates of click event

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ app.directive('ngStylus', function(){
   return {
     restrict: 'A',
     link: function(scope, element){
-      //canvas teeth here
+
+      var ctx = element[0].getContext('2d'); //set up canvas
+      var tracking = false; //stores tracking status
+      var prevX, prevY; //previous coordinates
+
+      //add event listener to canvas (element)
+      element.bind('mousedown', function(event){
+        prevX = event.offsetX; //where offsetX and Y are coordinates
+        prevY = event.offsetY; //from top left of canvas element
+
+        ctx.beginPath(); //begin tracing path
+        tracking = true; //tracking status to true
+      });
+
+
     }
   };
 });
