933570c4eba0558cde21f8145ce5cd3f34293782
Rohan <sarith21@gmail.com>
2014-07-28 21:41:07 -0700

    Adds scaffolding for custom directive to draw on canvas

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ var app = angular.module('app', []);
 app.run(function($rootScope){
   $rootScope.name = 'Tubby';
 });
+
+app.directive('ngStylus', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, element){
+      //canvas teeth here
+    }
+  };
+});
