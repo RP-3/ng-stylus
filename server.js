d2fc99e939e2f7ad09ec61e116c2f2911f973cc3
Rohan <sarith21@gmail.com>
2014-07-31 15:17:39 -0700

    Adds a simple server to recieve and save training data from demo app

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var csv = require('ya-csv');
+
+var app = express();
+
+var writer = new csv.CsvWriter(process.stdout);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + 'index.html'));
+app.use(express.static(__dirname));
+app.use(function(req, res){
+  if(Array.isArray(req.body)){
+    writer.writeRecord(req.body);
+  }
+});
+
+
+app.listen(3000);
